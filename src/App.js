92c5fe3b696d07cdab6b0898d389ffb66c27fb5a
joao <joao.dev94@gmail.com>
2023-11-11 21:14:31 -0300

    chore(page-navigation): update App to add PageNavigation component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import AppContainer from "./components/AppContainer";
 import "./index.css";
 import CardList from './components/CardList';
 import SearchBar from "./components/SearchBar";
+import PageNavigation from "./components/PageNavigation";
 import fetchData from "./services/apiService";
 
 const App = () => {
   const [apiData, setApiData] = useState([]);
   const [filteredApps, setFilteredApps] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchApiData = async () => {
@@ -27,12 +29,27 @@ const App = () => {
       return app.name.toLowerCase().includes(term.toLowerCase());
     });
     setFilteredApps(filteredApp);
+    setCurrentPage(1);
   }
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const itemsPerPage = 12;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const visibleApps = filteredApps.length > 0 ? filteredApps.slice(startIndex, endIndex) : apiData.slice(startIndex, endIndex);
 
   return (
     <AppContainer>
       <SearchBar onSearch={handleSearch}/>
-      <CardList apps={filteredApps.length > 0 ? filteredApps : apiData }/>
+      <CardList apps={visibleApps}/>
+      <PageNavigation
+        currentPage={currentPage}
+        totalPages={Math.ceil(filteredApps.length > 0 ? filteredApps.length / itemsPerPage : apiData.length / itemsPerPage)}
+        onPageChange={handlePageChange}
+      />
     </AppContainer>
   );
 }
