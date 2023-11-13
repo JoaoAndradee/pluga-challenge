63d2ab7efee16dd3c937cf326bc08fee58f46d4f
joao <joao.dev94@gmail.com>
2023-11-13 16:36:57 -0300

    chore(structure): install react-router-dom and refactor project base for routing

--- src/App.js
+++ src/App.js
@@ -1,88 +1,14 @@
-import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router";
 
-import AppContainer from "./components/AppContainer";
-import CardList from './components/CardList';
-import Modal from "./components/Modal";
-import PageNavigation from "./components/PageNavigation";
-import SearchBar from "./components/SearchBar";
-import fetchData from "./services/apiService";
+import Home from "./pages/Home";
 
-import "./index.css";
+import "./styles/index.css";
 
 const App = () => {
-  const [apiData, setApiData] = useState([]);
-  const [filteredApps, setFilteredApps] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [selectedApp, setSelectedApp] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [lastVisitedApps, setLastVisitedApps] = useState([]);
-
-  useEffect(() => {
-    const fetchApiData = async () => {
-      try {
-        const data = await fetchData();
-        setApiData(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    fetchApiData();
-  }, []);
-
-  const handleOpenModal = (app) => {
-    setSelectedApp(app);
-    setIsModalOpen(true);
-
-    const isAppInList = lastVisitedApps.some((visitedApp) => visitedApp.app_id === app.app_id);
-
-    if (!isAppInList) {
-    const updatedVisitedApps = [...lastVisitedApps, app].slice(-3);
-    setLastVisitedApps(updatedVisitedApps);
-    }
-  }
-
-  const handleCloseModal = () => {
-    setSelectedApp(null);
-    setIsModalOpen(false);
-  }
-
-  const handleSearch = (term) => {
-    const filteredApp = apiData.filter((app) => {
-      return app.name.toLowerCase().includes(term.toLowerCase());
-    });
-    setFilteredApps(filteredApp);
-    setCurrentPage(1);
-  }
-
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const itemsPerPage = 12;
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const visibleApps = filteredApps.length > 0 ? filteredApps.slice(startIndex, endIndex) : apiData.slice(startIndex, endIndex);
-
   return (
-    <AppContainer>
-      <SearchBar onSearch={handleSearch}/>
-      <CardList
-        apps={visibleApps}
-        onOpenModal={handleOpenModal}
-      />
-      <PageNavigation
-        currentPage={currentPage}
-        totalPages={Math.ceil(filteredApps.length > 0 ? filteredApps.length / itemsPerPage : apiData.length / itemsPerPage)}
-        onPageChange={handlePageChange}
-      />
-      <Modal
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-        app={selectedApp}
-        lastVisitedApps={lastVisitedApps}
-      />
-    </AppContainer>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
   );
 }
 
